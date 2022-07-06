9f10d2aa42a0c1ee0663bcc461f0cbd7d7655842
derpierre65 <git@derpierre65.de>
2022-07-07 01:15:33 +0200

    fix SIGTERM signal

--- start.sh
+++ start.sh
@@ -7,9 +7,9 @@ ROLE=${ROLE:-app}
 cd /usr/app/
 
 if [ "$ROLE" = "supervisor" ]; then
-  node supervisor.js
+  exec node supervisor.js
 elif [ "$ROLE" = "manager" ]; then
-  node channel-manager.js
+  exec node channel-manager.js
 else
   echo "Unsupported role: $ROLE"
   exit 1
